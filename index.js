2fa0c17ea4aaa2d545b0f13ab5b95a8961be8689
DavidWells <hello@davidwells.io>
2025-02-01 13:46:18 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,131 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+// Read and parse the CloudFormation template
+const template = JSON.parse(fs.readFileSync('./dirty-cloudformation.json', 'utf8'))
+
+// Function to recursively remove aws:cdk:path and cfn_nag from Metadata
+function cleanMetadata(obj) {
+  if (obj && typeof obj === 'object') {
+    if (obj.Metadata) {
+      // Remove aws:cdk:path
+      if (obj.Metadata['aws:cdk:path']) {
+        delete obj.Metadata['aws:cdk:path']
+      }
+      // Remove cfn_nag
+      if (obj.Metadata.cfn_nag) {
+        delete obj.Metadata.cfn_nag
+      }
+      // Remove empty Metadata objects
+      if (Object.keys(obj.Metadata).length === 0) {
+        delete obj.Metadata
+      }
+    }
+
+    // Recursively process all properties
+    for (const key in obj) {
+      cleanMetadata(obj[key])
+    }
+  }
+}
+
+// Function to remove CDK Metadata resources
+function removeCdkMetadata(template) {
+  const resources = template.Resources
+  for (const key in resources) {
+    if (resources[key].Type === 'AWS::CDK::Metadata') {
+      delete resources[key]
+    }
+  }
+}
+
+// Function to transform intrinsic functions to shorthand
+function transformIntrinsicFunctions(obj) {
+  if (obj && typeof obj === 'object') {
+    // Handle arrays
+    if (Array.isArray(obj)) {
+      return obj.map((item) => transformIntrinsicFunctions(item))
+    }
+
+    // Create new object to store transformed key-values
+    const transformed = {}
+
+    for (const [key, value] of Object.entries(obj)) {
+      // Transform the known intrinsic functions to shorthand
+      switch (key) {
+        case 'Fn::Join':
+          transformed['Ref::Join'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::Sub':
+          transformed['Ref::Sub'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::GetAtt':
+          // Convert from array format to dot notation
+          if (Array.isArray(value)) {
+            transformed['Ref::GetAtt'] = value.join('.')
+          } else {
+            transformed['Ref::GetAtt'] = value
+          }
+          break
+        case 'Fn::Select':
+          transformed['Ref::Select'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::Split':
+          transformed['Ref::Split'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::FindInMap':
+          transformed['Ref::FindInMap'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::If':
+          transformed['Ref::If'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::Not':
+          transformed['Ref::Not'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::Equals':
+          transformed['Ref::Equals'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::And':
+          transformed['Ref::And'] = transformIntrinsicFunctions(value)
+          break
+        case 'Fn::Or':
+          transformed['Ref::Or'] = transformIntrinsicFunctions(value)
+          break
+        case 'Ref':
+          transformed['Ref::Ref'] = value
+          break
+        default:
+          // Recursively transform nested objects
+          transformed[key] = transformIntrinsicFunctions(value)
+      }
+    }
+    return transformed
+  }
+  return obj
+}
+
+// Clean the template
+cleanMetadata(template)
+removeCdkMetadata(template)
+
+// Transform intrinsic functions
+const transformedTemplate = transformIntrinsicFunctions(template)
+
+// Convert to YAML
+let yamlContent = yaml.dump(transformedTemplate, {
+  indent: 2,
+  lineWidth: -1, // Prevent line wrapping
+  noRefs: true, // Handle circular references
+  noArrayIndent: true, // Prevent extra indentation for arrays
+})
+
+// Replace our temporary Ref:: prefix with ! for CloudFormation functions
+yamlContent = yamlContent.replace(/Ref::/g, '!')
+
+// Remove the colon after intrinsic function names
+yamlContent = yamlContent.replace(/!(Ref|GetAtt|Join|Sub|Select|Split|FindInMap|If|Not|Equals|And|Or):/g, '!$1')
+
+// Write the cleaned YAML to a file
+fs.writeFileSync('clean-cloudformation.yaml', yamlContent)
+
+console.log('Transformation complete! Output written to clean-cloudformation.yaml')
