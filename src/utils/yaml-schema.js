e5f302af669b46719a8ca338272b4a038e495660
DavidWells <hello@davidwells.io>
2025-02-05 17:03:02 -0800

    fix output

--- src/utils/yaml-schema.js
+++ src/utils/yaml-schema.js
@@ -3,10 +3,30 @@ const yaml = require('js-yaml')
 function getCfnSchema() {
   // Define CloudFormation tags schema
   const cfnTags = [
-    new yaml.Type('!Ref', { kind: 'scalar' }),
-    new yaml.Type('!Sub', { kind: 'scalar' }),
-    new yaml.Type('!GetAtt', { kind: 'scalar' }),
-    new yaml.Type('!Join', { kind: 'sequence' }),
+    new yaml.Type('!Ref', {
+      kind: 'scalar',
+      construct: function(data) {
+        return { 'Ref': data }
+      }
+    }),
+    new yaml.Type('!Sub', {
+      kind: 'scalar',
+      construct: function(data) {
+        return { 'Fn::Sub': data }
+      }
+    }),
+    new yaml.Type('!GetAtt', {
+      kind: 'scalar',
+      construct: function(data) {
+        return { 'Fn::GetAtt': data.split('.') }
+      }
+    }),
+    new yaml.Type('!Join', {
+      kind: 'sequence',
+      construct: function(data) {
+        return { 'Fn::Join': data }
+      }
+    }),
     new yaml.Type('!Select', { kind: 'sequence' }),
     new yaml.Type('!Split', { kind: 'sequence' }),
     new yaml.Type('!FindInMap', { kind: 'sequence' }),
